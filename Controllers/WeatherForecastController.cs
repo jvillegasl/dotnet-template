2e03e5174321689ad91eb419b065527b59d6f22a
jvillegasl <jvillegasl@uni.pe>
2024-06-29 13:51:36 -0500

    initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ef_local_db.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+#pragma warning disable IDE0052 // Remove unread private members
+    private readonly ILogger<WeatherForecastController> _logger;
+#pragma warning restore IDE0052 // Remove unread private members
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        this._logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
